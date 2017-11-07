a7bcf66bb8d93abbea9973135d827f6097659067
TakajiMesser <takaji.messer@gmail.com>
2017-11-06 16:19:01 -0800

    Initial Commit

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace Sample
+{
+    [Activity(Label = "DataTableView Sample", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize, HardwareAccelerated = true, MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.main_toolbar);
+            SetSupportActionBar(toolbar);
+
+            var exampleOne = FindViewById<RelativeLayout>(Resource.Id.example_one);
+            exampleOne.Click += (s, e) =>
+            {
+                //StartActivity(new Intent(this, typeof(TripActivity)));
+            };
+
+            SetUpDataTableView();
+        }
+
+        private void SetUpDataTableView()
+        {
+            
+        }
+    }
+}
+
